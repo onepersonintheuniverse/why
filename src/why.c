ce3f141039f884e0997fb3aa3f6a989bd907b689
onepersonintheuniverse <tokkivanc13@gmail.com>
2025-07-10 15:58:16 +0300

    added control flow

--- src/why.c
+++ src/why.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "stacks.h"
+#include <stacks.h>
 typedef long long s64;
 
 DECL_WHY_STACK_POP(s64)
@@ -31,14 +31,11 @@ char *match(char *str, char lb, char rb) {
     return str;
 }
 
-char *substr(char *str, int start, int end) {
+char *substr(char *start, char *end) {
     int len = end-start+1;
     char *new = malloc(len+1);
-    for (char *i = str+start; i < str+end; ++i) {
-        *new = *i;
-        ++new;
-    }
-    *new -= len;
+    *(new+len) = 0;
+    strncpy(new, start, len);
     return new;
 }
 
@@ -47,22 +44,26 @@ char *substr(char *str, int start, int end) {
  * @param prog the string which contains the program to run.
  */
 void interpret(char *prog) {
-    int bracketed = 0;
-    int escaped = 0;
+    int bracketed[4] = {};
+    char *p = prog;
     for (; *prog; ++prog) {
-        if (bracketed) {
-            if (*prog == '"' || *prog == '#') bracketed = 0;
+        if (bracketed[0] || bracketed[1] || bracketed[2] || bracketed[3]) {
+            if (*prog == '"' && bracketed[0]) --bracketed[0];
+            if (*prog == '#' && bracketed[1]) --bracketed[1];
+            if (*prog == ']' && bracketed[2]) --bracketed[2];
+            if (*prog == ')' && bracketed[3]) --bracketed[3];
             continue;
         }
         else if (*prog == '"') {
             char *rq = match(prog, '"', '"');
-            bracketed = 1;
+            ++bracketed[0];
             push(&st, 0);
             for (char *i = rq-1; i > prog; --i) {
                 if (*i != '\\') push(&st, *i);
                 else {
                     if (*(st.st_en-1) == 'n') *(st.st_en-1) = '\n';
                     if (*(st.st_en-1) == 't') *(st.st_en-1) = '\t';
+                    if (*(st.st_en-1) == '\\') *(st.st_en-1) = '\\';
                 }
             }
         }
@@ -72,6 +73,10 @@ void interpret(char *prog) {
         }
         else if (*prog == 'p') {
             printf("%lld", pop_s64(&st));
+        }
+        else if (*prog == 'g') {
+            s64 x; scanf("%lld", &x);
+            push_s64(&st, x);
         }
         else if (*prog == '#') {
             char *rh = match(prog, '#', '#');
@@ -83,7 +88,7 @@ void interpret(char *prog) {
             }
             x *= s;
             push_s64(&st, x);
-            bracketed = 1;
+            ++bracketed[1];
         }
         else if (*prog == '+') {
             push_s64(&st, pop_s64(&st) + pop_s64(&st));
@@ -97,19 +102,82 @@ void interpret(char *prog) {
         else if (*prog == '/') {
             push_s64(&st, pop_s64(&st) / pop_s64(&st));
         }
+        else if (*prog == '[') {
+            char *rb = match(prog, '[', ']');
+            char *q = substr(prog+1, rb-1);
+            while (pop_s64(&st)) interpret(q);
+            ++bracketed[2];
+        }
+        else if (*prog == '(') {
+            char *rb = match(prog, '(', ')');
+            char *q = substr(prog+1, rb-1);
+            s64 m = pop_s64(&st);
+            for (s64 i = 0; i < m; ++i)
+                interpret(q);
+            ++bracketed[3];
+        }
+        else if (*prog == '<') {
+            push_s64(&st, pop_s64(&st) < pop_s64(&st));
+        }
+        else if (*prog == '>') {
+            push_s64(&st, pop_s64(&st) > pop_s64(&st));
+        }
+        else if (*prog == '=') {
+            push_s64(&st, pop_s64(&st) == pop_s64(&st));
+        }
+        else if (*prog == '&') {
+            push_s64(&st, pop_s64(&st) && pop_s64(&st));
+        }
+        else if (*prog == '|') {
+            push_s64(&st, pop_s64(&st) || pop_s64(&st));
+        }
+        else if (*prog == '!') {
+            push_s64(&st, !pop_s64(&st));
+        }
+        else if (*prog == '^') {
+            s64 a = pop_s64(&st);
+            s64 b = pop_s64(&st);
+            push_s64(&st, a);
+            push_s64(&st, b);
+        }
+        else if (*prog == ';') {
+            s64 a = pop_s64(&st);
+            s64 b = pop_s64(&st);
+            s64 c = pop_s64(&st);
+            push_s64(&st, b);
+            push_s64(&st, a);
+            push_s64(&st, c);
+        }
+        else if (*prog == '$') {
+            s64 a = pop_s64(&st);
+            push_s64(&st, a);
+            push_s64(&st, a);
+        }
     }
 }
 
 int main(int argc, char const *argv[]) {
+    if (argc >= 2) {
+        int stsz = 4096;
+        if (argc >= 3) {
+            sscanf(*(argv+2), "%d", &stsz);
+        }
+        init_stack(&st, 8*stsz);
+        FILE *fp = fopen(*(argv+1), "r");
+        // find how long the file is and dump it all in a char *
+        fseek(fp, 0, SEEK_END);
+        length = ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+        char *buffer = malloc(length+1);
+        *(buffer+length) = 0;
+        fread(buffer, 1, length, fp);
+        interpret(buffer);
+        return 0;
+    }
     init_stack(&st, 4096);
-    FILE *fp = fopen("./examples/helloworld.why", "r");
-    // find how long the file is and dump it all in a char *
-    fseek(fp, 0, SEEK_END);
-    length = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
-    char *buffer = malloc(length+1);
-    *(buffer+length) = 0;
-    fread(buffer, 1, length, fp);
-    interpret(buffer);
-    return 0;
+    while (1) {
+        printf("\nkwhy> ");
+        char *q = malloc(1024); fgets(q, 1024, stdin);
+        interpret(q);
+    }
 }
